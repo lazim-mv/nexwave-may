f3dcad1fa17453f9957a71db147e0ce40bbe4c40
lazim-mv <106281909+lazim-mv@users.noreply.github.com>
2023-05-16 22:23:45 +0530

    v1

--- src/pages/index.js
+++ src/pages/index.js
@@ -106,7 +106,6 @@ export default function Home() {
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
         />
-        {/* <link rel="preload" href="/bgDarkKid.png" as="image" /> */}
       </Head>
 
       <div className="topContainer">
