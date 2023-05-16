89318cfaa46a4cc63f550db4c46d804aaec6687b
lazim-mv <106281909+lazim-mv@users.noreply.github.com>
2023-05-16 22:28:20 +0530

    v2

--- src/pages/components/Testimonials.jsx
+++ src/pages/components/Testimonials.jsx
@@ -14,10 +14,10 @@ function Testimonials() {
               <div className="flexbox">
                 <div className="fheader"></div>
                 <div className="fbody">
-                  "The team at Procube was a pleasure to work with. Their
+                  The team at Procube was a pleasure to work with. Their
                   expertise, and attention to detail were great. Exceeded our
                   expectations and we highly recommend them. Thank you for your
-                  hard work and dedication!"
+                  hard work and dedication!
                   <br></br>
                   <br></br>
                   <br></br>
@@ -33,12 +33,12 @@ function Testimonials() {
               <div className="flexbox">
                 <div className="fheader"></div>
                 <div className="fbody">
-                  "I just wanted to give a shoutout to the team at Procube for
+                  I just wanted to give a shoutout to the team at Procube for
                   being so easy to approach and work with. They were always
                   available to answer my questions and were super patient with
                   me, even when I had a million things going on. It was a
                   pleasure working with such a friendly and approachable team.
-                  Thanks guys!"
+                  Thanks guys!
                 </div>
                 <br></br>
                 <div className="text">
@@ -52,12 +52,12 @@ function Testimonials() {
               <div className="flexbox">
                 <div className="fheader"></div>
                 <div className="fbody">
-                  &ldquo;Procube completely transformed our business with their
-                  careful redesign of my business. They helped us to not only
-                  improve the look of our brand, but also to streamline our
-                  processes and increase our profits. We've seen tremendous
-                  growth since working with them and can't thank them enough for
-                  their help. Highly recommend!&rdquo;
+                  Procube completely transformed our business with their careful
+                  redesign of my business. They helped us to not only improve
+                  the look of our brand, but also to streamline our processes
+                  and increase our profits. We&apos;ve seen tremendous growth
+                  since working with them and can&apos;t thank them enough for
+                  their help. Highly recommend!
                 </div>
                 <br></br>
                 <div className="text">
@@ -74,5 +74,3 @@ function Testimonials() {
 
 export default Testimonials;
 
-{
-}
